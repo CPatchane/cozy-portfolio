d8b04b9a2f47cede8b9cd004e87f9052fa20d91f
Cédric Patchane Lacane <patchane.cedric@gmail.com>
2015-11-26 22:48:34 +0100

    Adding potential new views routes in the application router (portfolio, preferences (user choices) and accountSettings (for DouYouBuzz and OpenBadge settings)

--- client/app/lib/router.js
+++ client/app/lib/router.js
@@ -2,10 +2,25 @@ var application = require('application');
 
 module.exports = Backbone.Router.extend({
   routes: {
-    '': 'home'
+    '': 'home',
+    'portfolio': 'portfolio',
+    'preferences': 'preferences',
+    'accountSettings': 'accountSettings'
   },
 
   home: function () {
     $('body').html(application.homeView.render().el);
-  }
+  },
+  
+  portfolio: function () {
+    $('body').html(application.portfolioView.render().el);
+  },
+  
+  preferences: function () {
+    $('body').html(application.preferencesView.render().el);
+  },
+  
+  accountSettings: function () {
+    $('body').html(application.accountSettingsView.render().el);
+  },
 });
