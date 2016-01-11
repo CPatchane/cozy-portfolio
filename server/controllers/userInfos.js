93e0ee2603e5622e43071cc922369d7cec3ac3ff
Cédric Patchane Lacane <patchane.cedric@gmail.com>
2016-01-12 00:50:51 +0100

    Développement de la partie profil où quelques informations peuvent être récupérées depuis DoYouBuzz directement via un bouton (et une confirmation puisqu'il va écraser si des données sont déjà entrées)

--- server/controllers/userInfos.js
+++ server/controllers/userInfos.js
@@ -1,6 +1,8 @@
 var user = require('../models/user');
+var accounts = require('../models/accounts');
+var request = require("request");
 
-//fist entry of the client application
+//first entry of the client application
 module.exports.start = function(req, res, next){
   res.render('index.jade', {}, function(err, html) {
     res.send(html);
@@ -20,33 +22,115 @@ module.exports.list = function(req, res, next) {
 
 // We define another route that will handle user infos update (and first creation)
 module.exports.update = function(req, res, next) {
-  if(req.params.id === null){ //first time execution -> so we create it
-    user.create(req.body, function(err) {
+  var bodyArguments = JSON.parse(req.body.data);
+  var data = {
+    "firstName" : {"value": bodyArguments.firstName, "display": bodyArguments.firstNameCB},
+    "lastName" : {"value": bodyArguments.lastName, "display": bodyArguments.lastNameCB},
+    "birthdayDate" : {"value": bodyArguments.birthdayDate, "display": bodyArguments.birthdayDateCB},
+    "email" : {"value": bodyArguments.email, "display": bodyArguments.emailCB},
+    "description" : {"value": bodyArguments.description, "display": bodyArguments.descriptionCB},
+    "status" : {"value": bodyArguments.status, "display": bodyArguments.statusCB},
+    "localisation" : {"value": bodyArguments.localisation, "display": bodyArguments.localisationCB},
+    "activeResumeId" : bodyArguments.activeResumeId || "",
+    "hobbies" : {"value": bodyArguments.hobbies, "display": bodyArguments.hobbiesCB},
+    "keywords" : {"value": bodyArguments.keywords, "display": bodyArguments.keywordsCB}
+  }
+  if(req.body.id == ""){ //first time execution -> so we create it
+    user.create(data, function(err) {
       if(err !== null) {
-        next(err);
+        res.status(500).send("ERROR");
       }
       else {
-        res.redirect('back');
+        res.status(200).send("Données sauvegardées");
       }
     });
   }else{
-    user.find(req.params.id, function(err, user) {
+    user.find(req.body.id, function(err, user) {
       if(err !== null) {
-        next(err);
+        res.status(500).send("ERROR");
       }
       else if(user === null) {
         res.status(404).send("User infos not found. ERROR");
       }
       else {
-        user.updateAttributes(req.body, function(err) {
+        user.updateAttributes(data, function(err) {
           if(err !== null) {
-            next(err);
+            res.status(500).send("ERROR");
           }
           else {
-            res.redirect('back');
+            res.status(200).send("Changements sauvegardés");
           }
         });
       }
     });
   }
+};
+
+module.exports.getFromDYB = function(req, res, next) {
+  accounts.all(function(err, accountsInfos) {
+    if(err !== null) {
+      next(err);
+    }
+    else {
+      accountsInfos = accountsInfos[0];
+      if(!accountsInfos.doYouBuzzOauthVerifierToken || !accountsInfos.doYouBuzzOauthVerifierTokenSecret){
+        res.status(500).send("Erreur : Veuillez connecter votre compte DoYouBuzz dans les paramètres puis réssayer.");
+      }else{
+        var oauth =
+        { consumer_key: accountsInfos.doYouBuzzAPIKey,
+        consumer_secret: accountsInfos.doYouBuzzAPISecret,
+        token: accountsInfos.doYouBuzzOauthVerifierToken,
+        token_secret: accountsInfos.doYouBuzzOauthVerifierTokenSecret
+        };
+        var url = 'https://api.doyoubuzz.com/user'
+        request.get({url:url, oauth:oauth, json:true}, function (e, r, data) {
+          //path of profil photo with data.avatars.big
+          //console.log(data)
+          user.all(function(err, userInfos) {
+            if(err !== null) {
+              res.status(500).send("Veuillez d'abord entrer des données et les valider une première fois.");
+            }
+            else {
+              userInfos = userInfos[0];
+              var dataToUpdate = {
+                "firstName": {"value": data.user.firstname, "display": userInfos.firstName.display},
+                "lastName": {"value": data.user.lastname, "display": userInfos.lastName.display},
+                "email": {"value": data.user.email, "display": userInfos.email.display},
+                "resumes": data.user.resumes.resume,
+              }
+              if(userInfos.id == ""){ //first time execution -> so we create it
+                user.create(dataToUpdate, function(err) {
+                  if(err !== null) {
+                    res.status(500).send("ERROR");
+                  }
+                  else {
+                    res.status(200).send("Données récupérées et sauvegardées");
+                  }
+                });
+              }else{
+                user.find(userInfos.id, function(err, user) {
+                  if(err !== null) {
+                    res.status(500).send("ERROR");
+                  }
+                  else if(user === null) {
+                    res.status(404).send("User infos not found. ERROR");
+                  }
+                  else {
+                    user.updateAttributes(dataToUpdate, function(err) {
+                      if(err !== null) {
+                        res.status(500).send("ERROR");
+                      }
+                      else {
+                        res.status(200).send("Données récupérées et sauvegardées");
+                      }
+                    });
+                  }
+                });
+              }
+            }
+          });
+        })
+      }
+    }
+  });
 };
