ea28a781031569fb85a57d897fd8a5075b96143e
Cédric Patchane Lacane <patchane.cedric@gmail.com>
2016-01-25 18:28:21 +0100

    Function to add document functional

--- server/controllers/portfolioDocuments.js
+++ server/controllers/portfolioDocuments.js
@@ -16,16 +16,14 @@ module.exports.list = function(req, res, next) {
 
 // We define a new route that will handle document creation
 module.exports.add = function(req, res, next) {
-  console.log(req.body);
-  res.status(200).send()
-  /*portfolioDocument.create(req.body, function(err, portfolioDocument) {
+  portfolioDocument.create(req.body, function(err, portfolioDocument) {
     if(err !== null) {
-      next(err);
+      res.status(500).status("Erreur serveur pour l'ajout du document");
     }
     else {
       res.status(200).send("Document correctement ajouté.")
     }
-  });*/
+  });
 };
 
 // We define another route that will handle document deletion
